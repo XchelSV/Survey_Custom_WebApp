39008d5166cdf23425f391e536122e63eb2a1f64
Xchel_Sanchez <xchelsvz@gmail.com>
2016-09-05 15:33:06 -0500

    DS Favicon

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'icon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(multipart());
 app.use(session({
 
-        secret: 'q~!!#s4HALA^MADRIDcds4<>>*S3--_-`´ç@',
+        secret: 'qwertyuiop',
         saveUninitialized:  false,
         resave:       false
       }));
